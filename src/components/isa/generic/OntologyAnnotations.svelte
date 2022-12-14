1cb1fdeb9f68c9c0cea15b8cc69bd4e9dfe3dfe7
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2022-12-14 18:10:05 +0100

    Cleaned OntologyAnnotations component, added feature to delete OntologyTerms

--- src/components/isa/generic/OntologyAnnotations.svelte
+++ src/components/isa/generic/OntologyAnnotations.svelte
@@ -1,64 +1,27 @@
 <script lang="ts">
     export let ontologyAnnotations: Array<Object>;
 
-    import ontologyLookup from '@/lib/ontologyLookup';
-
-    import Comment from '@/components/isa/generic/Comment.svelte';
     import Svelecte from 'svelecte';
 
+    import ontologyLookup from '@/lib/ontologyLookup';
     import Schemas from '@/lib/schemas.js';
-    import Assay from '@/components/isa/assay/Assay.svelte';
 
+    import Comment from '@/components/isa/generic/Comment.svelte';
 
-    const addRole = () => {
-        comments = [...comments, {name: '', value: ''}];
-    }
-
-    /*function handleRemoveComment(event) {
-        comments.splice(event.detail.index, 1);
-        comments = [...comments];
-    }*/
-
-    let payload = null;
-
-    function onSubmit(e) {
-        const object = {};
-        const formData = new FormData(e.target);
-        formData.forEach((value, key) => {
-            if (object[key]) {
-            object[key] += ', ' + value;
-            return;
-            }
-            object[key] = value
-        });
-        payload = JSON.stringify(object, null, 2);
-    }
-
-    function resetPayload(e) {
-        payload = null;
-    }
+    let roles;
 
     async function handleFetch(query) {
-        console.log(query);
         let result = await ontologyLookup(query);
         result = result.map(item => {
             
             item.origName = item.Name;
             item.Name = '<b>'+item.Name+'</b> ['+item.Accession+']';
 
-            /*return {
-                'name': item.Name+'['+item.Accession+']',
-                'acc': item.Accession
-            }*/
             return item;
         });
-        console.log(result);
-        console.log(result[0][1]);
+
         return result;
     }
-
-    let roles;
-    $: console.log(roles);
 
     const addRoles = async () => {
         let emptyOA = await Schemas.getObjectFromSchema('ontology_annotation');
@@ -68,11 +31,14 @@
             _emptyOA.termAccession = role.Accession;
             _emptyOA.annotationValue = role.origName;
 
-            console.log(_emptyOA);
             ontologyAnnotations = [...ontologyAnnotations, _emptyOA];
-            console.log(ontologyAnnotations);
         }
         roles = [];
+    }
+
+    function onDelete(index) {
+        ontologyAnnotations.splice(index, 1);
+        ontologyAnnotations = [...ontologyAnnotations];
     }
 
 </script>
@@ -83,34 +49,22 @@
 
     <ul>
     {#each ontologyAnnotations as oa, index}
-        <li><b>{@html oa.annotationValue}</b> [{oa.termAccession}]</li>
+        <li><b>{@html oa.annotationValue}</b> [{oa.termAccession}] <button on:click={() => onDelete(index)}>X</button></li>
     {/each}
     </ul>
 
-    <!--<input type /> <button on:click|preventDefault={() => addRole()}>add role</button>-->
+    <Svelecte name="selection"
+        bind:value={roles}
+        valueAsObject={true}
+        multiple
+        required
+        placeholder="Search for OntologyTerm"
+        fetch={handleFetch}
+    />
 
-    
-
-    <form action="" on:submit|preventDefault={onSubmit}>
-        <Svelecte name="selection"
-            bind:value={roles}
-            on:change={resetPayload}
-            valueAsObject={true}
-            multiple
-            required
-            placeholder="Search for OntologyTerm"
-            fetch={handleFetch}
-        >
-        </Svelecte>
-
-        <div class="mt-2">
-          <button on:click|preventDefault={() => addRoles()} class="btn btn-success">Add roles</button>
-        </div>
-
-        {#if payload}
-          <pre>{payload}</pre>
-        {/if}
-    </form>
+    <div class="mt-2">
+        <button on:click|preventDefault={() => addRoles()} class="btn btn-success">Add roles</button>
+    </div>
 
 </section>
 
