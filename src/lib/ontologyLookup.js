1f3d44389a641e098fe2cdf448ac617e7223f102
Dennis Psaroudakis <dennis.psaroudakis@gmail.com>
2022-12-14 10:50:12 +0100

    add ontologyLookup JS feature

---
+++ src/lib/ontologyLookup.js
@@ -0,0 +1,13 @@
+function ontologyLookup(search_term, ontology = null, n_terms = 7) {
+    return fetch('https://swate.nfdi4plants.org/api/IOntologyAPIv2/getTermSuggestions', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: '[{"n": '+n_terms+',"query": "'+search_term+'"}]'
+      })
+        .then((response) => response.json())
+}
