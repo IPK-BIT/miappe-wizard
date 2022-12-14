2fdde2a09f31c4c6170c4ac0bd8d0df577413c1f
Dennis Psaroudakis <dennis.psaroudakis@gmail.com>
2022-12-14 15:39:32 +0100

    implement ontology selection in REST lookup

--- src/lib/ontologyLookup.js
+++ src/lib/ontologyLookup.js
@@ -1,12 +1,17 @@
 function ontologyLookup(search_term, ontology = null, n_terms = 7) {
+    requestBody = { n: n_terms, query:search_term };
+    if(ontology != null) {
+        requestBody.ontology = ontology
+    }
     return fetch('https://swate.nfdi4plants.org/api/IOntologyAPIv2/getTermSuggestions', {
         method: 'POST',
         mode: 'cors',
         cache: 'no-cache',
         headers: {
-            'Content-Type': 'application/json',
-            'accept': 'application/json'
+          'Content-Type': 'application/json',
+          'accept': 'application/json'
         },
-        body: '[{"n": '+n_terms+',"query": "'+search_term+'"}]'
-    }).then((response) => response.json())
+        body: JSON.stringify([requestBody])
+      })
+        .then((response) => response.json())
 }
