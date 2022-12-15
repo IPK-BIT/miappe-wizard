2979ff4a42f90017d816399f9cbfdcbc644ab5fc
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2022-12-15 09:10:07 +0100

    Create Forms.svelte

---
+++ src/components/Forms.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let isaObj;
+    export let level;
+
+    import Studies from '@/components/isa/study/Studies.svelte';
+</script>
+
+
+<section>
+    Forms<br />
+    {level}<br />
+    {isaObj}<br /><hr />
+
+    {#if isaObj[level]}
+    <svelte:component this={Studies} bind:value={isaObj[level]} />
+    {/if}
+
+</section>
+
+
+<style>
+
+</style>
