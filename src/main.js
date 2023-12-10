9ead8fc069cb05da73684d183ff85e138ef8c8fb
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2023-12-10 07:54:58 +0100

    shadowDOM test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 //import './app.css'
 import App from './App.svelte'
 
+const target = document.querySelector('#app');
+//const shadowRoot = target.attachShadow({ mode: "open" });
+
 const app = new App({
-    target: document.getElementById('app')
+    //target: document.getElementById('app')
+    target: target
 })
 
 export default app
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [
+        svelte({
+            emitCss: false
+        })
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
