c66f1f8e4ff3abc8f620451ca1640f1299867e6d
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2023-12-11 17:52:57 +0100

    fix

--- src/components/InitView.svelte
+++ src/components/InitView.svelte
@@ -8,7 +8,7 @@
 
     async function startWizardMode() {
         if (Object.keys($isaObj).length == 0) {
-            let emptyInvestigation = await Schemas.getObjectFromSchema('investigation');
+            let emptyInvestigation = Schemas.getObjectFromSchema('investigation');
             $isaObj = emptyInvestigation;
         }
 
@@ -16,7 +16,7 @@
     }
 
     async function addInvestigation() {
-        let emptyInvestigation = await Schemas.getObjectFromSchema('investigation');
+        let emptyInvestigation = Schemas.getObjectFromSchema('investigation');
         $isaObj = emptyInvestigation;
 
         $appstate = appstate.FORM;
