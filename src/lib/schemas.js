b3b28af06b8b282a4fbbf05b9e524656620a9cb6
Manuel Feser <feser@ipk-gatersleben.de>
2023-12-14 17:07:12 +0100

    Intermediate push

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -15,6 +15,7 @@ import ontology_annotation_schema from '@/lib/schemas/ontology_annotation_schema
 import ontology_source_reference_schema from '@/lib/schemas/ontology_source_reference_schema.json';
 import person_schema from '@/lib/schemas/person_schema.json';
 import process_schema from '@/lib/schemas/process_schema.json';
+import process_parameter_value_schema from '@/lib/schemas/process_parameter_value_schema.json';
 import protocol_parameter_schema from '@/lib/schemas/protocol_parameter_schema.json';
 import protocol_schema from '@/lib/schemas/protocol_schema.json';
 import publication_schema from '@/lib/schemas/publication_schema.json';
@@ -38,6 +39,7 @@ const mapping = {
     ontology_source_reference: ontology_source_reference_schema,
     person: person_schema,
     process: process_schema,
+    process_parameter_value: process_parameter_value_schema,
     protocol_parameter: protocol_parameter_schema,
     protocol: protocol_schema,
     publication: publication_schema,
