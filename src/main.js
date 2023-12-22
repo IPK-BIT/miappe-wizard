8d0f65964bbf96532aa1d4d3628868e54119d228
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2023-12-22 16:44:47 +0100

    cleaning

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 //import './app.css'
 import App from './App.svelte'
 
-let config = {};
-
 const target = document.querySelector('#app');
 //const shadowRoot = target.attachShadow({ mode: "open" });
 
 function startApp(params) {
     const app = new App({
-        //target: document.getElementById('app')
         target: target,
         props: {
             config: params.config
         }
     });
+    return app;
 }
 
 window.miappeWizard = {
