2614846fe807939aa4bf1f0ed66d53f479274c7c
Patrick König <35546377+patrick-koenig@users.noreply.github.com>
2023-12-22 07:05:59 +0100

    fix: added copying of config file to /dist for npm run build

--- copy_data_to_dist.cjs
+++ copy_data_to_dist.cjs
@@ -2,32 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 const copyDir = (src, dest, callback) => {
- const copy = (copySrc, copyDest) => {
-   fs.readdir(copySrc, (err, list) => {
-     if (err) return callback(err);
+    const copy = (copySrc, copyDest) => {
+        fs.readdir(copySrc, (err, list) => {
+            if (err) return callback(err);
 
-     list.forEach((item) => {
-       const srcItem = path.resolve(copySrc, item);
-       const destItem = path.resolve(copyDest, item);
+            list.forEach((item) => {
+                const srcItem = path.resolve(copySrc, item);
+                const destItem = path.resolve(copyDest, item);
 
-       fs.stat(srcItem, (err, stat) => {
-         if (err) return callback(err);
+                fs.stat(srcItem, (err, stat) => {
+                    if (err) return callback(err);
 
-         if (stat.isFile()) {
-           fs.createReadStream(srcItem).pipe(fs.createWriteStream(destItem));
-         } else if (stat.isDirectory()) {
-           fs.mkdirSync(destItem, { recursive: true });
-           copy(srcItem, destItem);
-         }
-       });
-     });
-   });
- };
+                    if (stat.isFile()) {
+                        fs.createReadStream(srcItem).pipe(fs.createWriteStream(destItem));
+                    } else if (stat.isDirectory()) {
+                        fs.mkdirSync(destItem, { recursive: true });
+                        copy(srcItem, destItem);
+                    }
+                });
+            });
+        });
+    };
 
- fs.access(dest, (err) => {
-   if (err) fs.mkdirSync(dest, { recursive: true });
-   copy(src, dest);
- });
+    fs.access(dest, (err) => {
+        if (err) fs.mkdirSync(dest, { recursive: true });
+        copy(src, dest);
+    });
 };
 
+const copyFile = (sourcePath, destinationPath) => {
+
+    if (!fs.existsSync(destinationPath)) {
+        fs.mkdirSync(destinationPath, { recursive: true });
+    }
+    
+    const fileName = path.basename(sourcePath);
+    const destinationFile = path.join(destinationPath, fileName);
+    
+    fs.copyFile(sourcePath, destinationFile, (err) => {
+        if (err) {
+            console.error('Error copying file:', err);
+        } else {
+            console.log('File copied successfully!');
+        }
+    });
+}
+
 copyDir('./data', './dist/data');
+copyFile('./wizard.config.js', './dist/');
