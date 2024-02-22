f9f172fb7b31feadcc484dac9efd8c443f528718
arendd <arendd@ipk-gatersleben.de>
2024-02-22 14:46:21 +0100

    Update BreedFides config

--- config/breedfides/wizard.steps.config.js
+++ config/breedfides/wizard.steps.config.js
@@ -1,6 +1,6 @@
 window.steps = [
     {
-        title: 'Please provide the Title and Description of your Plant Phenotyping Project and when you submitted and published the Dataset.',
+        title: 'Please provide a Title and Description of your Dataset and which Institution or Organisation is responsible for.',
         fields: [
             {
                 label: 'Project Title',
@@ -21,50 +21,39 @@ window.steps = [
                 explanation: 'DM-4'
             },
             {
-                label: 'Submission Date',
-                type: 'date',
+                label: 'Institution',
+                type: 'text',
                 isaMapping: {
                     entity: 'investigation',
-                    attribute: 'submissionDate'
+                    attribute: 'comments',
+                    commentName: 'Study Contact Institution'
                 },
-                explanation: 'DM-5'
+                explanation: 'DM-16'
             },
             {
-                label: 'Public Release Date',
-                type: 'date',
+                label: 'Institution Address',
+                type: 'text',
                 isaMapping: {
                     entity: 'investigation',
-                    attribute: 'publicReleaseDate'
+                    attribute: 'comments',
+                    commentName: 'Study Contact Institution'
                 },
-                explanation: 'DM-6'
+                explanation: 'DM-16'
+            },
+            {
+                label: 'Country (PREFILL)',
+                type: 'text',
+                isaMapping: {
+                    entity: 'investigation',
+                    attribute: 'comments',
+                    commentName: 'Study Country'
+                },
+                explanation: 'DM-17'
             }
         ]
     },
-   /* {
-        title: 'What is the submission date of your plant phenotyping project?',
-        fields: [
-            {
-                label: 'Submission Date',
-                type: 'date',
-                isaMapping: {
-                    entity: 'investigation',
-                    attribute: 'submissionDate'
-                },
-                explanation: 'DM-5'
-            },
-            {
-                label: 'Public Release Date',
-                type: 'date',
-                isaMapping: {
-                    entity: 'investigation',
-                    attribute: 'publicReleaseDate'
-                },
-                explanation: 'DM-6'
-            }
-        ]
-    },*/
     {
-        title: 'Please provide the Authors of your Dataset and define their Contribution.',
+        title: 'Please provide all Authors of your Dataset and define their Contribution.',
         fields: [
             {
                // label: 'Project title',
@@ -78,36 +67,11 @@ window.steps = [
                     showComments: false
                 }
             }
-        ]
-    },
-    {
-        title: 'Please provide the responsible Institute for the whole Plant Phenotyping Project.',
-        fields: [
-            {
-                label: 'Contact address',
-                type: 'text',
-                isaMapping: {
-                    entity: 'investigation',
-                    attribute: 'comments',
-                    commentName: 'Study Contact Institution'
-                },
-                explanation: 'DM-16'
-            },
-            {
-                label: 'Country',
-                type: 'text',
-                isaMapping: {
-                    entity: 'investigation',
-                    attribute: 'comments',
-                    commentName: 'Study Country'
-                },
-                explanation: 'DM-17'
-            }
         ],
         hook: 'addStudy'
     },
     {
-        title: 'Please provide is the Title of your Experiment and the responsible Institution',
+        title: 'Please provide the Title and Location of your Study',
         fields: [
             {
                 label: 'Study title',
@@ -119,14 +83,34 @@ window.steps = [
                 }
             },
             {
-                label: 'Contact address',
+                label: 'Location',
                 type: 'text',
                 isaMapping: {
                     entity: 'study',
                     attribute: 'comments',
-                    commentName: 'Study Contact Institution'
+                    commentName: 'Study Experimental Site'
                 },
-                explanation: 'DM-16'
+                explanation: 'DM-18'
+            },
+            {
+                label: 'Location Latitude',
+                type: 'text',
+                isaMapping: {
+                    entity: 'study',
+                    attribute: 'comments',
+                    commentName: 'Study Latitude'
+                },
+                explanation: 'DM-19'
+            },
+            {
+                label: 'Location Longitude',
+                type: 'text',
+                isaMapping: {
+                    entity: 'study',
+                    attribute: 'comments',
+                    commentName: 'Study Longitude'
+                },
+                explanation: 'DM-20'
             },
             {
                 label: 'Country',
@@ -140,32 +124,7 @@ window.steps = [
             }
         ]
     },
-  /*  {
-        title: 'Please provide contact address of your institute.',
-        fields: [
-            {
-                label: 'Contact address',
-                type: 'text',
-                explanation: 'DM-16',
-                isaMapping: {
-                    entity: 'study',
-                    attribute: 'comments',
-                    commentName: 'Study Contact Institution'
-                }
-            },
-            {
-                label: 'Country',
-                type: 'text',
-                explanation: 'DM-17',
-                isaMapping: {
-                    entity: 'study',
-                    attribute: 'comments',
-                    commentName: 'Study Country'
-                }
-            }
-        ]
-    },*/
-    {
+ /*   {
         title: 'Please provide a link to the data file of the study.',
         fields: [
             {
@@ -194,9 +153,16 @@ window.steps = [
             protocolDescription: 'How the plants were grown up.',
             protocolParameters: ['Light intensity', 'Air temperature']
         }
-    },
+    },*/
     {
         title: 'Please provide a brief summary of the Growth Conditions.',
+        hook: 'addProtocol',
+        hookParameters: {
+            protocolName: 'Growth',
+            protocolVersion: 'MIAPPE v1.1',
+            protocolDescription: 'How the plants were grown up.',
+            protocolParameters: ['Light intensity', 'Air temperature']
+        },
         fields: [
             {
                 label: 'Growth description',
